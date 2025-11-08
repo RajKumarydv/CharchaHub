3433ed0e36cbdf64c236f06e3b0d1f3af795f3af
Raj <rajk70796@gmail.com>
2025-11-08 15:53:35 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
+import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.routes.js'
+
+
+dotenv.config();
+const app =  express();
+app.use(cookieParser());
+const PORT = process.env.PORT || 5001;
+
+
+// app.get('/api/auth/singup', (req, res) => {
+//   res.send('singup route');
+// });
+
+// app.get('/api/auth/login', (req, res) => {
+//   res.send('login route');
+// });
+//  app.get('/api/auth/logout', (req, res) => {
+//   res.send('logout route');
+// });
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users",userRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+  connectDB();
+});
