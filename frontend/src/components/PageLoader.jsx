98327d72ae62da60f42869dd934d22f13f430544
Raj <rajk70796@gmail.com>
2025-11-18 23:44:11 +0530

    create page loader component

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
-const PageLoader = () => {
+const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="min-h-screen">
+      <div className="flex">
+        {showSidebar && <Sidebar />}
 
-export default PageLoader
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Layout;
