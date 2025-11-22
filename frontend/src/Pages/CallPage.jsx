3e94c80930ab2c7b48ab145518772b6dde63de57
Raj <rajk70796@gmail.com>
2025-11-22 23:14:58 +0530

    create call page

--- frontend/src/Pages/CallPage.jsx
+++ frontend/src/Pages/CallPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { VideoIcon } from "lucide-react";
 
-const CallPage = () => {
+function CallButton({ handleVideoCall }) {
   return (
-    <div>
-      
+    <div className="p-3 border-b flex items-center justify-end max-w-7xl mx-auto w-full absolute top-0">
+      <button
+        onClick={handleVideoCall}
+        className="btn btn-success btn-sm text-white"
+      >
+        <VideoIcon className="size-6" />
+      </button>
     </div>
-  )
+  );
 }
 
-export default CallPage
+export default CallButton;
